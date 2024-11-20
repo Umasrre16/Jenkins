e97a26a854ea23fa91b9569c2fe8315cd9e39ee0
us <us@Uma>
2024-11-20 09:25:20 +0530

    PollSCMTest class Added

---
+++ Jenkins_Practice/src/test/java/pollscm/PollSCMTest.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void demo() {
+		Reporter.log("PollSCMTest class Executed", true);	
+	}
+}
